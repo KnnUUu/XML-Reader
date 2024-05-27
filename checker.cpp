cb5042749a8500f36e377861c42d1553f42d5cba
UUu <44579350+KnnUUu@users.noreply.github.com>
2024-05-27 21:24:50 +0900

    add test case

--- checker.cpp
+++ checker.cpp
@@ -15,6 +15,12 @@ int main(int argc, char *argv[])
         {"<Design><Code>hello world</Code></Design><People>", false},          // no closing tag for "People"
         {"<People><Design><Code>hello world</People></Code></Design>", false}, // "/Code" should come before "/People"
         {"<People age=”1”>hello world</People>", false},                       // there is no closing tag for "People age=”1”" and no opening tag for "/People"
+        {"<>hello world</>", false},                                           // empty tag treated invalid here
+        {"<Code>hello world", false},                                          // closing tag not exist
+        {"hello world</Code>", false},                                         // opening tag not exist
+        {"<Code><Code><Code>hello world</Code></Code></Code>", true},          // duplicate tag treated valid here
+        {"<CODE>hello world</code>", false},                                   // case mismatch
+        {"<Code></Code>", true},                                               // empty text treated valid here
     };
 
     int failed_count = 0;
